191104b757d2d8cf400ca3fa7c8c4eb2f6c3963d
martinkaldramov <martinkaldramov1@gmail.com>
2023-11-16 17:11:16 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+# Create ChromeOptions object to configure headless mode
+chrome_options = Options()
+chrome_options.add_argument("--headless=new")
+driver = webdriver.Chrome()
+app = FastAPI()
+with open("test.txt", 'w') as f:
+    f.write("")
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+@app.get("/write/{text}")
+async def root1(text):
+    with open("test.txt", 'w') as f:
+        f.write(text)
+    return {"message": text}
+@app.get("/read")
+async def root2():
+    with open("test.txt", 'r') as f:
+        content = f.read()
+    return {"message": content}
+# Persistent Disk Test?
